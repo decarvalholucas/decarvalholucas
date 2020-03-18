7fb1e83d3f9cfd29755fd2edfaf93315d49390e8
Lucas de Carvalho <lucasdecarvalho@spacebr-pc.livreeleve.ljv>
2020-03-18 15:00:51 -0300

    Added analyctis plugin and nprogress

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,12 +8,18 @@ const siteMetadata = require("./config/metadata")
 
 module.exports = {
   siteMetadata,
-  pathPrefix: "/decarvalholucas",
+  pathPrefix: "/blog",
   plugins: [
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `tomato`,
+      },
+    },
     {
       resolve: `gatsby-plugin-styled-components`,
       options: {
-        displayName: process.env.NODE_ENV !== 'production',
+        displayName: process.env.NODE_ENV !== "production",
       },
     },
     {
@@ -22,6 +28,20 @@ module.exports = {
         path: `${__dirname}/src/pages`,
         ignore: [`**/styles.js`],
       },
-    }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-161156696-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+      },
+    },
   ],
 }
