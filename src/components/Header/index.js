7a47f06e9a00883246c92bf5804895d942ece373
Lucas de Carvalho <lucasdecarvalho.ti@gmail.com>
2020-03-23 17:44:41 -0300

    Remove temporarily transitionPortal

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,27 +7,25 @@ export default function Header() {
   return (
     <>
       <GlobalStyle />
-      <TransitionPortal level="top">
-        <LCHeader>
-          <div className="container">
-            <LinkTranstionUp to="/" className="logo">
-              <span>decarvalholucas</span>
-            </LinkTranstionUp>
-            <div className="menu">
-              <div>
-                <ul>
-                  <li>
-                    <LinkTranstionUp to="/">Home</LinkTranstionUp>
-                  </li>
-                  <li>
-                    <LinkTranstionDown to="/about">About</LinkTranstionDown>
-                  </li>
-                </ul>
-              </div>
+      <LCHeader>
+        <div className="container">
+          <LinkTranstionUp to="/" className="logo">
+            <span>decarvalholucas</span>
+          </LinkTranstionUp>
+          <div className="menu">
+            <div>
+              <ul>
+                <li>
+                  <LinkTranstionUp to="/">Home</LinkTranstionUp>
+                </li>
+                <li>
+                  <LinkTranstionDown to="/about">About</LinkTranstionDown>
+                </li>
+              </ul>
             </div>
           </div>
-        </LCHeader>
-      </TransitionPortal>
+        </div>
+      </LCHeader>
     </>
   )
 }
